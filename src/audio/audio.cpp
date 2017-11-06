c2f11f18a8213658923e0b474fec82ac3a08d3f1
Alex Michon <alexmichon@hotmail.fr>
2017-11-06 14:19:14 -0800

    Multiple channels

--- src/audio/audio.cpp
+++ src/audio/audio.cpp
@@ -6,24 +6,24 @@
 #include <qendian.h>
 
 
-#define MAX_AMPLITUDE 60
+#define MAX_AMPLITUDE 40
 #define MAX_VALUE 32000
 
 void audio_to_rgb(unsigned char *buffer, int buffer_size, long rate, RGB *leds, int leds_strips, int leds_columns) {
-    fftw_complex *in = (fftw_complex *) malloc(sizeof(fftw_complex) * buffer_size);
-    for (int i = 0; i < buffer_size; i++) {
-        in[i][0] = buffer[i];
-        in[i][1] = 0;
+    fftw_complex *in = (fftw_complex *) malloc(sizeof(fftw_complex) * buffer_size/2);
+    for (int i = 0; i < buffer_size/2; i++) {
+        in[i][0] = buffer[2*i];
+        in[i][1] = buffer[2*i+1];
     }
-    fftw_complex *out = (fftw_complex *) malloc(sizeof(fftw_complex) * buffer_size);
-    fftw_plan plan = fftw_plan_dft_1d(buffer_size, in, out, FFTW_FORWARD, FFTW_ESTIMATE);
+    fftw_complex *out = (fftw_complex *) malloc(sizeof(fftw_complex) * buffer_size/2);
+    fftw_plan plan = fftw_plan_dft_1d(buffer_size/2, in, out, FFTW_FORWARD, FFTW_ESTIMATE);
     fftw_execute(plan);
 
 
     double *levels = (double *) malloc(sizeof(double) * leds_strips);
-    for (int i = 0; i < buffer_size/2; i++) {
+    for (int i = 0; i < buffer_size/4; i++) {
         double mag = 2 * sqrt(out[i][0]*out[i][0] + out[i][1]*out[i][1]) / buffer_size;
-        double freq = i * (double)rate / buffer_size;
+        double freq = i * (double)rate / buffer_size/2;
 
         if (freq > 19 && freq <= 140) {
             if (mag > levels[0]) {
@@ -52,10 +52,11 @@ void audio_to_rgb(unsigned char *buffer, int buffer_size, long rate, RGB *leds, 
         }
     }
 
+/*
     for (int i = 0; i < leds_strips; i++) {
-        //qDebug("Level %d: %.2f", i, levels[i]);
+        qDebug("Level %d: %.2f", i, levels[i]);
     }
-
+*/
     for (int i = 0; i < leds_strips; i++) {
         levels[i] /= MAX_AMPLITUDE;
         if (levels[i] >= 1) { levels[i] = 1; }
@@ -63,7 +64,7 @@ void audio_to_rgb(unsigned char *buffer, int buffer_size, long rate, RGB *leds, 
 
     for (int i = 0; i < leds_strips; i++) {
         for (int j = 0; j < leds_columns; j++) {
-            leds[i*leds_columns + j].r = 255;
+            leds[i*leds_columns + j].r = (int) (255 * (1 - pow(levels[i], leds_columns - j)));
             leds[i*leds_columns + j].g = (int) (255 * (1 - pow(levels[i], leds_columns - j)));
             leds[i*leds_columns + j].b = (int) (255 * (1 - pow(levels[i], leds_columns - j)));
         }
