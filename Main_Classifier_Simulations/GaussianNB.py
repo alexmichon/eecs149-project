adbdba1e94287b4233e4bba2b8b25596ee2817fb
Alex Michon <alexmichon@hotmail.fr>
2017-11-12 23:26:20 -0800

    Use batch of samples

--- Main_Classifier_Simulations/GaussianNB.py
+++ Main_Classifier_Simulations/GaussianNB.py
@@ -9,19 +9,50 @@ from sklearn.model_selection import train_test_split
 
 from mlxtend.plotting import plot_decision_regions
 
+import random
 
+
+# Loads data 
+# Each line is a sample
+# Params:	test_size: percentage of samples used for testing
+#			random_state: random value to split train and test samples
 def load_data(test_size, random_state):
+	# X are samples, Y are targets
 	X_left, Y_left = data_parser.parse("TestData/left", 0)
 	X_right, Y_right = data_parser.parse("TestData/right", 1)
 	X_stop, Y_stop = data_parser.parse("TestData/stop", 2)
 
+	# Concatentates values from each file
 	X = np.concatenate((X_left, X_right, X_stop))
 	Y = np.concatenate((Y_left, Y_right, Y_stop))
 
 	return train_test_split(X, Y, test_size=test_size, random_state=random_state)
 
 
+# Loads data by batches
+# Params:	batch_size: number of lines to create one sample
+#			test_size: percentage of samples used for testing
+#			random_state: random value to split train and test samples
+def load_batches(batch_size, test_size, random_state):
+	# X are samples, Y are targets
+	X_left, Y_left = data_parser.parse_batch("TestData/left", 0, batch_size)
+	X_right, Y_right = data_parser.parse_batch("TestData/right", 1, batch_size)
+	X_stop, Y_stop = data_parser.parse_batch("TestData/stop", 2, batch_size)
 
+	# Concatentates values from each file
+	X = np.concatenate((X_left, X_right, X_stop))
+	Y = np.concatenate((Y_left, Y_right, Y_stop))
+
+	return train_test_split(X, Y, test_size=test_size, random_state=random_state)
+
+
+# Plot learning curbe
+# Params: 	estimator: model (here GaussianNB)
+#			title: title of the graph
+#			X: samples
+#			Y: targets
+#			ylim: limit of the Y axis
+#			cv: cross-validation generator
 def plot_learning_curve(estimator, title, X, y, ylim=None, cv=None):
 	plt.figure()
 	plt.title(title)
@@ -45,8 +76,32 @@ def plot_learning_curve(estimator, title, X, y, ylim=None, cv=None):
 	return plt
 
 
+# Test with a given number of rows per sample
+# Returns the score on the test samples
+# Params: 	batch_size: number of lines to create one sample
+def test_batches(batch_size):
+	X_train, X_test, Y_train, Y_test = load_batches(batch_size, 0.3, random.randint(1,100))
+
+	# Train model on train samples
+	clf = GaussianNB()
+	clf.fit(X_train, Y_train.ravel())
+
+	# Test model on test samples
+	return clf.score(X_test, Y_test)
+
+
 if __name__ == "__main__":
-	X_train, X_test, Y_train, Y_test = load_data(0.3, 42)
+
+	# Test on batch sizes from 1 to 10
+	for i in range(1,11):
+		# Run the test 100 times to get an average value 
+		scores = 0
+		for j in range(100):
+			scores += test_batches(i)
+		print("Batch size: {0}\tScore: {1:.2f}%".format(i, scores))
+
+	'''
+	X_train, X_test, Y_train, Y_test = load_batches(5, 0.3, 42)
 
 	clf = GaussianNB()
 	clf.fit(X_train, Y_train.ravel())
@@ -61,5 +116,10 @@ if __name__ == "__main__":
 	plot_learning_curve(estimator, title, X_train, Y_train, cv=cv)
 	plt.show()
 
-	plot_decision_regions(X_train, Y_train, clf=clf, filler_feature_values={2: 1, 3: 1, 4: 1, 5: 1})
+	filler_feature_values = {}
+	for i in range(2, X_train.shape[1]):
+		filler_feature_values[i] = 2
+
+	plot_decision_regions(X_train, Y_train, clf=clf, filler_feature_values=filler_feature_values)
 	plt.show()
+	'''
--- Main_Classifier_Simulations/data_parser.py
+++ Main_Classifier_Simulations/data_parser.py
@@ -2,6 +2,10 @@ import numpy as np
 import csv
 
 
+# Parse data line by line
+# Each line is a sample
+# Params:	path: path to the data file
+#			type: type of the values (left, right or stop)
 def parse(path, type):
 	print("Parsing data from " + path)
 
@@ -10,11 +14,49 @@ def parse(path, type):
 	with open(path, newline='') as file:
 		reader = csv.reader(file, delimiter=',')
 		for row in reader:
+			# if row is not empty
 			if row:
+				# append the row to the list of samples
 				a = np.array([row[:-1]])
 				data = np.append(data, a, axis=0)
 
+	# Create a target array full of the given type
 	target = np.full((data.shape[0],), type)
+
+	return data.astype(np.float), target
+
+
+# Parse data by batch
+# Each first 'nb_rows' rows are aligned to be a unique sample of 6*nb_rows values
+# Params: 	path: path to the data file
+#			type: type of the values (left, right or stop)
+#			nb_rows: Number of rows used to create one sample
+def parse_batch(path, type, nb_rows):
+	data = np.empty((0,6*nb_rows))
+
+	index = 0
+	row_count = 0
+	sample = np.zeros((1, 6*nb_rows))
+
+	with open(path, newline='') as file:
+		reader = csv.reader(file, delimiter=',')
+		for row in reader:
+			# if row is not empty and if the batch is not full
+			if row and (row_count < nb_rows):
+				# append the data to the sample
+				sample[0][row_count * 6:(row_count+1)*6] = np.array([row[:-1]])
+				row_count += 1
+			elif not row:
+				# if row empty it is the end of the sample
+				# the sample is added to the list
+				data = np.append(data, sample, axis=0)
+				index += 1
+				row_count = 0
+				sample = np.zeros((1, 6*nb_rows))
+
+	# Create a target array full of the given type
+	target = np.full((data.shape[0],), type)
+
 	return data.astype(np.float), target
 
 
@@ -22,3 +64,5 @@ if __name__ == "__main__":
 	parse("TestData/left", "left")
 	parse("TestData/right", "right")
 	parse("TestData/stop", "stop")
+
+	parse_batch("TestData/left", "left", 5)
