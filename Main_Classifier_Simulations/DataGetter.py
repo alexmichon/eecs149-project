b9ead5ff503db3ef416f88e919ebc216c347b813
WanNJ <omyjackwan@gmail.com>
2017-11-13 13:45:10 -0800

    GestureDetector Simulation Completed

--- Main_Classifier_Simulations/DataGetter.py
+++ Main_Classifier_Simulations/DataGetter.py
@@ -3,6 +3,7 @@
 import csv
 import numpy as np
 
+
 class Config(object):
     """
     Define a class to store parameters
@@ -15,6 +16,7 @@ class Config(object):
         self.batch_size = batch_size
         self.overlap_size = overlap_size
         self.new_data_size = batch_size - overlap_size
+
 
 class TestDataGetter(object):
     """
@@ -29,15 +31,34 @@ class TestDataGetter(object):
         self.X_left = self.__get_x_data_matrix("TestData/left")
         self.X_right = self.__get_x_data_matrix("TestData/right")
         self.X_stop = self.__get_x_data_matrix("TestData/stop")
+        self.X_none = self.__get_x_data_matrix("TestData/none")
 
-    def get_x_data(self):
 
-        X = np.concatenate((self.X_left, self.X_right, self.X_stop))
+    def get_x_data(self, only_gst_data=True):
+        """
+        Get the x data from the data set by requirement set in the init function
 
-        return X
+        :param only_gst_data: if set to false, then it will return all the data including non-gesture data
+        :return: parsed data matrix
+        """
+        if only_gst_data:
+            return np.concatenate((self.X_left, self.X_right, self.X_stop))
+        else:
+            return np.concatenate((self.X_none, self.X_left, self.X_right, self.X_stop))
 
-    def get_y_data(self):
+    def get_y_data(self, only_gst_data=True):
+        """
+        Get the target/y data from the data set by requirement set in the init function
+
+        :param only_gst_data: if set to false, then it will return all the data including non-gesture data
+        :return: parsed target class array
+        """
         y = []
+
+        if not only_gst_data:
+            for i in range(self.X_none.shape[0]):
+                y.append(-1)
+
         for i in range(self.X_left.shape[0]):
             y.append(0)
         for i in range(self.X_right.shape[0]):
@@ -49,14 +70,13 @@ class TestDataGetter(object):
 
         return y
 
-
     def __get_x_data_matrix(self, file_path):
         print("Getting Data From " + file_path + " ...")
 
         result_matrix = []
 
         with open(file_path, newline='') as testfile:
-            buffer = [] # Store the overlap data
+            buffer = []  # Store the overlap data
             reader = csv.reader(testfile, delimiter=',')
 
             for row in reader:
